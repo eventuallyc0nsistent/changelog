5a03e2dd48959101354f8e306bd95ccd54324b4a
Kiran Koduru <crazycreature11@gmail.com>
2016-05-23 02:19:27 -0400

    Initialize project
    Added code for parsing git logs and printing CHANGELOG to command line

---
+++ changelog/command_line.py
@@ -0,0 +1,72 @@
+import re
+import argparse
+from datetime import datetime
+from subprocess import Popen, PIPE
+
+def parse_logs(logs, fields):
+    logs = logs.strip('\n\x1e').split("\x1e")
+    logs = [row.strip().split("\x1f") for row in logs]
+    logs = [dict(zip(fields.keys(), row)) for row in logs]
+    return logs
+
+def write_logs(new_version, logs):
+    date_today = datetime.now().strftime('%d %b %Y')
+    print '###{0} ({1})'.format(new_version, date_today)
+    for item in logs:
+        title = item['subject']
+        issues = re.findall(issue_id_pattern, title + item['body'])
+        if issues:
+            for issue in issues:
+                if ('Merge branch' in title) or ('Merge pull' in title):
+                    continue
+                print '- {0} ([{1}](https://company.com/browse/{1}))'.format(title, issue)
+
+        else:
+            if ('Merge branch' in title) or ('Merge pull' in title):
+                continue
+            print '- {0}'.format(title)
+
+def main(from_, to, new_version, issue_id_pattern):
+    # fields to fetch from commit log
+    # For format parameters refer: https://git-scm.com/docs/pretty-formats
+    fields = {
+        'id': '%H',
+        'author_name': '%an',
+        'author_email': '%ae',
+        'date': '%ad',
+        'subject': '%s',
+        'body': '%b'
+    }
+    format_str = '%x1f'.join(fields.values()) + '%x1e'
+
+    command = 'git log {0}...{1} --format="{2}"'.format(from_, to, format_str)
+    p = Popen(command, shell=True, stdout=PIPE)
+    (logs, _) = p.communicate()
+    logs = parse_logs(logs, fields)
+    write_logs(new_version, logs)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Publish CHANGELOG from git commits')
+    parser.add_argument('-s', '--start', type=str, required=True, 
+                        help='Enter commit/tag/branch you want CHANGELOG from')
+    parser.add_argument('-e', '--end', type=str, 
+                        help='Enter commit/tag/branch you want CHANGELOG till. \
+                              Default value is set to master')
+    parser.add_argument('-v', '--version', type=str, required=True, 
+                        help='Enter new version number for the CHANGELOG')
+    parser.add_argument('-i', '--issue', type=str, 
+                        help='Enter JIRA issue ID pattern. Default set to EKDATA-\d+')
+
+    args = parser.parse_args()
+    from_ = args.start
+    to_ = args.end
+    new_version = args.version
+    issue_id_pattern = args.issue
+
+    # set defaults
+    if not to_:
+        to_ = 'master'
+    if not issue_id_pattern:
+        issue_id_pattern = r'#\d+'
+
+    main(from_, to_, new_version, issue_id_pattern)
